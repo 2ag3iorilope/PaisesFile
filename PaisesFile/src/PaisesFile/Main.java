4b23017b16214e20c338a2896af69c91b32bd16d
2ag3.iorilope <2ag3.iorilope@Enara-p22.mshome.net>
2024-10-03 09:31:33 +0200

    Last

---
+++ PaisesFile/src/PaisesFile/Main.java
@@ -0,0 +1,227 @@
+package PaisesFile;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		menua();		
+
+	}
+	
+	public static void menua() {
+		
+		 String hasierakoLanDirektorioString;
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Sartu zure lan direktorioaren helbide absolutua:");
+	        
+	        hasierakoLanDirektorioString = sc.nextLine();
+	        
+	        //Konprobatu hutsik dagoen gure entry-a
+	        if (hasierakoLanDirektorioString.isEmpty()) {
+	            hasierakoLanDirektorioString = ".";
+	        }
+	        
+	        // Erakutsi zein direktorio erabiliko den
+	        System.out.println("Lan direktorioa: " + hasierakoLanDirektorioString);
+	        
+	        // File objetua sortu drektorioarekin
+	        File directory = new File(hasierakoLanDirektorioString);
+	        
+	        //Konprobatu ea existitzen den direktorioa
+	        if (directory.exists() && directory.isDirectory()) {
+	            System.out.println("Zure direktorioa existitzen da: " + hasierakoLanDirektorioString + " eta direktorioa " + directory.getAbsolutePath()) ;
+	        } else {
+	            System.out.println("Errorea: Zure direktorioa ez da existitzen edo ez da zuzeneko direktorioa.Beraz defektuzko lan direktorioa erabiliko da");
+	            hasierakoLanDirektorioString = ".";
+	            directory =  new File(hasierakoLanDirektorioString);
+	            System.out.println(directory.getAbsolutePath());
+	        
+	        }
+	        
+	        
+	        int Aukera = 0;
+
+	        do {
+	            // Erakutsi Menua
+	            System.out.println("\n----- MENUA -----");
+	            System.out.println("1. Erakutsi Lan direktorioko fitxategiak eta Direktorioak");
+	            System.out.println("2. Aldatu lan direktorioa Direktorio berri bat sortuz");
+	            System.out.println("3. Gorde Herrialdeak Fitxategietan");
+	            System.out.println("4. Ezabatu fitxategiak .aad");
+	            System.out.println("5.Irten");
+	            System.out.print("Elige una opción: ");
+	            
+	           
+	            Aukera = sc.nextInt();
+	            sc.nextLine();
+
+	            switch(Aukera) {
+	                case 1:
+	                    
+	                	 // Konproabtu fitxategiak dauden
+	                    File[] fitxategiakFiles = directory.listFiles();
+	                    
+	                    if (fitxategiakFiles != null && fitxategiakFiles.length > 0) {
+	                        System.out.println("Fitxategiak eta direktorioak:");
+
+	                      
+	                        System.out.printf("%-20s %-10s %10s %-30s %n", "Data", "Mota", "Tamaina", "Izena");
+	                        System.out.println("----------------------------------------------------------------------");
+
+	                        //ERakutsi fixtategi eta direktorio bakoitza modu egokian
+	                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+	                        for (File fitxategiFile : fitxategiakFiles) {
+	                            //Modifikazio data
+	                            String data = sdf.format(fitxategiFile.lastModified());
+
+	                            // Konproabtu direktorioa den
+	                            String mota = fitxategiFile.isDirectory() ? "<DIR>" : "     ";
+
+	                            // Fitxategien tamaina lortu
+	                            long tamaina = fitxategiFile.isFile() ? fitxategiFile.length() : 0;
+
+	                            //Informazioa erakutsi formatu egokian
+	                            System.out.printf("%-20s %-10s %10s %-30s %n", data, mota, tamaina  > 0 ? tamaina : "", fitxategiFile.getName());
+	                        }
+	                    } else {
+	                        System.out.println("Ez dago fitxategirik edo direktoriarik.");
+	                    }
+	                    break;
+	        
+	
+	                    
+	                case 2:
+	                    // Direktorio berria eskatu
+	                    System.out.println("Sartu zure lan direktorio berriaren izena:");
+	                    String berriLanDirektorioIzena = sc.nextLine();
+
+	                    // Konproabtu direktorioa ya existitzen den
+	                    File berriLanDirektorio = new File(directory, berriLanDirektorioIzena); // Sortu direktorio berria gure direktorioan
+
+	                    if (berriLanDirektorio.exists()) {
+	                        System.out.println("Errorea: Direktorioa '" + berriLanDirektorioIzena + "' dagoeneko existitzen da.");
+	                    } else {
+	                       
+	                        boolean sortuDa = berriLanDirektorio.mkdir();
+	                        //Sortu bada erakutsi sortu dela
+	                        if (sortuDa) {
+	                            System.out.println("Direktorio berria sortu da: " + berriLanDirektorio.getAbsolutePath());
+	                            //Direktorioa aldatu
+	                            directory = berriLanDirektorio;
+	                            System.out.println("Direktorio berria : " + directory.getAbsolutePath());
+	                        } else {
+	                            System.out.println("Errorea: Ezin izan da sortu '" + berriLanDirektorioIzena + "' direktorioa.");
+	                        }
+	                    }
+	                    break;
+
+	                    
+	                case 3:
+	                	
+	                	ArrayList<PaisesFile> paises = new ArrayList<>();
+	            		
+	            		paises.add(new PaisesFile("31", "Holanda","Amsterdam", 15460000.00,78, LocalDate.of(1581, 7, 26),false ));
+
+	            		paises.add(new PaisesFile("376", "Andorra", "Andorra La Vieja",64000.00, 0, LocalDate.of(1993, 3, 14),true ));
+
+	            		paises.add(new PaisesFile("90", "Turquia","Ankara",61058000.00,67, LocalDate.of(1923, 10, 29),true  ));
+
+	            		paises.add(new PaisesFile("261", "Madagascar","Antananarivo",13651000.00, 52, LocalDate.of(1960, 6, 26),true ));
+
+	            		paises.add(new PaisesFile("685", "Samoa Occidental","Apia",165000.00, 68, LocalDate.of(1962, 1, 1),true  ));
+
+	            		paises.add(new PaisesFile("213", "Argelia","Argel", 27959000.00, 70, LocalDate.of(1962, 7, 5),true ));
+
+	            		paises.add(new PaisesFile("291", "Eritrea","Asmara", 3400000.00, 0, LocalDate.of(1993, 5, 24),false ));
+
+	            		paises.add(new PaisesFile("595", "Paraguay","Asuncion", 4828000.00, 68, LocalDate.of(1825, 8, 25),true ));
+
+	            		paises.add(new PaisesFile("30", "Grecia","Atenas",10467000.00, 78, LocalDate.of(1830, 2, 3), true));
+
+	            		paises.add(new PaisesFile("964", "Irak","Bagdad",20097000.00, 66, LocalDate.of(1958, 7, 14),false  ));
+	            		
+	                  
+	                    for (PaisesFile pais : paises) {
+	                    
+	                        String fileName = pais.gethiriizena() + ".aad"; // Fitxategiaren izena Hiriaren izenarekin
+	                        File paisFile = new File(directory, fileName); // Fitxategia sortu gure direktorioan
+
+	                        try {
+	                            // Sortu fitxategia
+	                            if (paisFile.createNewFile()) {
+	                                System.out.println("Fitxategia sortu da: " + paisFile.getAbsolutePath());
+	                            } else {
+	                                System.out.println("Fitxategia existitzen da: " + paisFile.getAbsolutePath());
+	                            }
+
+	                       
+	                            try (FileWriter writer = new FileWriter(paisFile)) {
+	                                writer.write("Hiriaren izena: " + pais.gethiriizena());
+	                                System.out.println("Hiriaren izena idatzi da zure fitxategian: " + pais.gethiriizena());
+	                            }
+
+	                        } catch (IOException e) {
+	                            System.out.println("Errorea hurrengo fitxategia sortzerakoan: " + pais.gethiriizena());
+	                            e.printStackTrace();
+	                        }
+	                    }
+	                    break;
+
+	                    
+	                case 4:
+	                    //Erakutsi momentuko fitxategiak
+	                    File[] fitxategiakk = directory.listFiles(); 
+
+	                    if (fitxategiakk != null && fitxategiakk.length > 0) {
+	                        for (File fitxategi : fitxategiakk) {
+	                            // Konprobatu .aad amaitzen dela
+	                            if (fitxategi.isFile() && fitxategi.getName().toLowerCase().endsWith(".aad")) {
+	                                // Fitxategia ezabatu
+	                                if (fitxategi.delete()) {
+	                                    System.out.println("Hurrengo fitxategia ezabatu da: " + fitxategi.getName());
+	                                } else {
+	                                    System.out.println("Ondorengo fitxategia borratzerakoan errorea egon da: " + fitxategi.getName());
+	                                }
+	                            }
+	                        }
+	                    } else {
+	                        System.out.println("Ez dago fitxategirik ezabatzeko.");
+	                    }
+	                    break;
+	                
+	                case 5 :
+	                	
+	                	System.out.println("Irteten....");
+	                	break;
+	                default:
+	                    System.out.println("Aukera desegokia aukeratu ondo.");
+	            }
+
+	        } while (Aukera != 5);
+	        
+	        sc.close(); 
+	    }
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	}
+
+
---
+++ PaisesFile/src/PaisesFile/PaisesFile.java
@@ -0,0 +1,222 @@
+package PaisesFile;
+
+import java.time.LocalDate;
+
+
+/**
+ * The Class Paises.
+ */
+public class PaisesFile {
+
+	/** Hiriaren Telefono kodea. */
+	String PaisKode;
+
+	/** Hiriaren izena. */
+	String paissString;
+
+	/** Kapitalaren izena. */
+	String kapitalaString;
+
+	/** Hiriko poblazio kopurua. */
+	Double poblazioaDouble;
+
+	/**  Bizi esperantza Kopurua. */
+	int BiziEsperantzakop;
+
+	/** Hiria sortutako Data. */
+	LocalDate Sortudata;
+
+	/** Demokrazia dagoen Hiri horretan. */
+	boolean Demokrazioabool;
+
+
+	/**
+	 * Instantiates a new paises.
+	 *
+	 * @param kodea , hiriko telefono kodea
+	 * @param Pais , hiriaren izea
+	 * @param Kapitala , hiriko kapitala
+	 * @param Poblazioa, hiriko poblazioa
+	 * @param Bizi_Esperantza , hiriko bizi esperantza
+	 * @param sortudataDate , hiria sortu zen data
+	 * @param Demokrazia , demokrazia dagoen hirian
+	 */
+	public PaisesFile(String kodea,String Pais,String Kapitala,Double Poblazioa,int Bizi_Esperantza,LocalDate sortudataDate, boolean Demokrazia ) {
+
+		PaisKode = kodea;
+		paissString = Pais;
+		kapitalaString = Kapitala;
+		poblazioaDouble = Poblazioa;
+		BiziEsperantzakop = Bizi_Esperantza;
+		Sortudata = sortudataDate;
+		Demokrazioabool = Demokrazia;
+
+	}
+
+
+
+	/**
+	 * Hiriko telefono kodea lortzen duen metodoa.
+	 *
+	 * @return Hiriko Telefono Kodea
+	 */
+	public String getPaisKode() {
+		return PaisKode;
+	}
+
+
+
+	/**
+	 * Hiriko telefono kodea ezartzen duen metodoa.
+	 *
+	 * @param paisKode , telefono kode berria
+	 */
+	public void setPaisKode(String paisKode) {
+		PaisKode = paisKode;
+	}
+
+	/**
+	 * Hiriaren sortu zen data lortzen duen metodoa.
+	 *
+	 * @return hiria sortu zen data
+	 */
+	public LocalDate lortudata() {
+		return Sortudata;
+	}
+
+	/**
+	 * Hiraren data ezartzen duen metodoa.
+	 *
+	 * @param Hiriaren data berria
+	 */
+	public void setData(LocalDate fecha) {
+		this.Sortudata = fecha;
+	}
+
+
+	/**
+	 * Hiriaren izena lortzen duen metodoa.
+	 *
+	 * @return  paiss , Hiriaren izena
+	 */
+	public String getPaissString() {
+		return PaisKode;
+	}
+
+	/**
+	 * Hiriaren sortu zen data lortzen duen metodoa.
+	 *
+	 * @return hiria sortu zen data
+	 */
+	public LocalDate getFecha() {
+		return Sortudata;
+	}
+
+	/**
+	 * Hiriaren izena ezarten duen metodoa.
+	 *
+	 * @param paissString , hiriaren izen berria
+	 */
+	public void setPaissString(String paissString) {
+		this.paissString = paissString;
+	}
+	
+	public String gethiriizena() {
+		return paissString;
+	}
+
+
+	/**
+	 * Hiriaren kapitala lortzeko metodoa.
+	 *
+	 * @return  kapitala , Hiriaren kaptalaren izena
+	 */
+	public String getKapitalaString() {
+		return kapitalaString;
+	}
+
+
+	/**
+	 * Hiriaren kapitala ezartzeko metodoa.
+	 *
+	 * @param kapitalaString, hiriaren kapital berria
+	 */
+	public void setKapitalaString(String kapitalaString) {
+		this.kapitalaString = kapitalaString;
+	}
+
+	/**
+	 * Hiriaren poblazioa lortzen duen metodoa.
+	 *
+	 * @return the poblazioa , hiriaren poblazio guztia
+	 */
+	public Double getPoblazioaDouble() {
+		return poblazioaDouble;
+	}
+
+
+	/**
+	 * Hiriaren poblazioa ezartzeko metodoa.
+	 *
+	 * @param poblazioaDouble , hiriaren poblazio berria
+	 */
+	public void setPoblazioaDouble(Double poblazioaDouble) {
+		this.poblazioaDouble = poblazioaDouble;
+	}
+
+
+	/**
+	 * Hiriaren bizi esperantza lortzeko metodoa.
+	 *
+	 * @return hiriaren bizi esperantza
+	 */
+	public int getBiziEsperantzakop() {
+		return BiziEsperantzakop;
+	}
+
+
+	/**
+	 * Hiriaren bizi esperantza ezartzeko metodoa.
+	 *
+	 * @param biziEsperantzakop , hiriaren bizi esperantza berria
+	 */
+	public void setBiziEsperantzakop(int biziEsperantzakop) {
+		BiziEsperantzakop = biziEsperantzakop;
+	}
+
+	/**
+	 * Hiriak demokrazia duen konprobatzen duen metodoa
+	 *
+	 * @return true, demokrazia badu
+	 */
+	public boolean isDemokrazioabool() {
+		return Demokrazioabool;
+	}
+
+	/**
+	 * Gets the paiss string.
+	 *
+	 * @return the paiss string
+	 */
+	public String getPaissStringa() {
+		return paissString; 
+	}
+
+	/**
+	 * To string.
+	 *
+	 * @return erregistroa formatu egokian
+	 */
+	@Override
+	public String toString() {
+		return "Kodea: " + PaisKode + "\n" +
+				"Estatua: " + paissString + "\n" +
+				"BiziEsperantza: " + (BiziEsperantzakop != 0 ? BiziEsperantzakop : "Ez dago eskuragarri") + "\n" +
+				"Data Sortu: " + Sortudata + "\n" +
+				"Poblazioa: " + poblazioaDouble + "\n" +
+				"Kapitala: " + kapitalaString + "\n" +
+				"Demokrazia: " + (Demokrazioabool ? "Bai" : "Ez") + "\n" + 
+				"------------------------------";
+	}
+
+}
